14ac681518637c365836b81457b3bdc9121a08c4
Elysia Gabe <elysiagabe@gmail.com>
2020-03-20 08:50:12 -0700

    Sets up basic component structure , initializes application state and fetches data from server

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Smurf = () => {
+    return (
+        <div>
+            I am just one smurf. 
+        </div>
+    )
+}
+
+export default Smurf;
---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Smurf from './Smurf';
+
+const SmurfList = () => {
+    return (
+        <div>
+            I'm the Smurf List
+            <Smurf />
+        </div>
+    )
+}
+
+export default SmurfList;
