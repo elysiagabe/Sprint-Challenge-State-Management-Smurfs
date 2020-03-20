d34034ad20e8c0fcef7ee616129e276848d0ab13
Elysia Gabe <elysiagabe@gmail.com>
2020-03-20 09:46:01 -0700

    Creates form and adds add new smurf functionality

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
+// context
 import { SmurfContext } from '../contexts/SmurfContext';
+// components
 import SmurfList from './SmurfList';
+import SmurfForm from './SmurfForm';
 
 function App() {
   const [smurfs, setSmurfs] = useState([]);
@@ -16,16 +19,32 @@ function App() {
     .catch(err => console.log('There was an error fetching data from the server: ', err))
   }, [])
 
+  const addNewSmurf = smurf => {
+    const newSmurf = {
+      id: Date.now(),
+      name: smurf.name,
+      age: smurf.age,
+      height: smurf.height
+    }
+    axios.post('http://localhost:3333/smurfs', newSmurf)
+    .then(res => {
+      console.log('Success!', res)
+      setSmurfs([...smurfs, newSmurf])
+    })
+    .catch(err => console.log('There was an error', err))
+  }
+
   return (
-    <div className="App">
-      <h1>SMURFS! 2.0 W/ Context API</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
-      <SmurfContext.Provider value={smurfs}>
+    <SmurfContext.Provider value={{ smurfs, addNewSmurf }}>
+      <div className="App">
+        <h1>SMURFS! 2.0 W/ Context</h1>
+        
+        <SmurfForm />
+        
         <SmurfList />
-      </SmurfContext.Provider>
-    </div>
+        
+      </div>
+    </SmurfContext.Provider>
   );
 }
 
--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -3,12 +3,11 @@ import Smurf from './Smurf';
 import { SmurfContext } from '../contexts/SmurfContext';
 
 const SmurfList = () => {
-    const smurfs = useContext(SmurfContext);
+    const { smurfs } = useContext(SmurfContext);
     //console.log('smurfs in smurflist.js', smurfs)
 
     return (
         <div>
-            I'm the Smurf List
             {smurfs.map(smurf => {
                 return (
                     <Smurf 
